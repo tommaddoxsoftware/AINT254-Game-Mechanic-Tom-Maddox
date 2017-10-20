cc137bcfcfa19244a85c53f139cff0aadeba0568
tommaddoxsoftware <tommaddoxsoftware@users.noreply.github.com>
2017-10-20 15:34:11 +0100

    Workshop progress added
    Physics session! Hinge joints/shooting and addForce!

---
+++ AINT254 Game Mechanic Prototype/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Rigidbody m_rigidbody;
+    private float m_force = 1000;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody.AddForce(transform.forward * m_force);
+
+        Destroy(gameObject, 5.0f);
+	}
+	
+}
---
+++ AINT254 Game Mechanic Prototype/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    [SerializeField] private GameObject m_bullet;
+    private Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+        m_transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(m_bullet, m_transform.position + m_transform.forward * 3, m_transform.rotation);
+        }
+	}
+}
